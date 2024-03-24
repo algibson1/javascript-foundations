bf79f6e2719395bb94f341ebd106a5c5ae8b9477
Artemy Gibson <artemygibson@gmail.com>
2024-03-24 14:24:43 -0700

    Add all mythical creature functions

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,41 @@
+function createDragon(name, rider, temperment) {
+  var dragon = {
+    name: name,
+    rider: rider,
+    temperment: temperment,
+    timesEaten: 0,
+    hungry: true
+  }
+  return dragon;
+}
 
+function greetRider(dragon) {
+  // var greeting = 'Hi, ' + dragon.rider + '!';
+  // return greeting;
+  return `Hi, ${dragon.rider}!` // << note BACKTICKS not quotes!!
+}
+
+function eat(dragon) {
+  dragon.timesEaten += 1;
+  if (dragon.timesEaten === 3) {
+    dragon.hungry = false;
+  }
+  return dragon;
+}
+
+function findFireBreathers(dragonArray) {
+  var fireBreathers = []
+  dragonArray.forEach(function(dragon) {
+    if (dragon.temperment === 'aggressive') {
+      fireBreathers.push(dragon);
+    }
+  })
+  return fireBreathers;
+}
 
 module.exports = {
-  // createDragon, 
-  // greetRider, 
-  // eat, 
-  // findFireBreathers
+  createDragon,
+  greetRider,
+  eat, 
+  findFireBreathers
 }
--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -1,9 +1,57 @@
+function createHobbit(name, age) {
+  var isAdult = (age >= 33) ? true : false
+  var isOld = (age >= 101) ? true : false
+  
+  var hobbit = {
+    name: name || 'unknown',
+    age: age || 0,
+    isAdult: isAdult,
+    isOld: isOld,
+    acquaintances: []
+  }
 
+  return hobbit;
+}
+
+function celebrateBirthday(hobbit) {
+  hobbit.age += 1;
+  if (hobbit.age === 33) {
+    hobbit.isAdult = true;
+  } else if (hobbit.age === 101) {
+    hobbit.isOld = true;
+  }
+  return hobbit;
+}
+
+function getRing(hobbit) {
+  if (hobbit.name === 'Frodo') {
+    return 'Here is the ring!';
+  } else {
+    return 'You can\'t have it!';
+  }
+}
+
+function meetPeople(hobbit, people) {
+  people.forEach(function(person) {
+    hobbit.acquaintances.push(person);
+  })
+  return hobbit;
+}
+
+function findFriends(hobbit) {
+  var friends = []
+  hobbit.acquaintances.forEach(function(person) {
+    if (person.relationship === 'friend') {
+      friends.push(person.name);
+    }
+  })
+  return friends;
+}
 
 module.exports = {
-  // createHobbit, 
-  // celebrateBirthday, 
-  // getRing, 
-  // meetPeople, 
-  // findFriends
+  createHobbit, 
+  celebrateBirthday, 
+  getRing, 
+  meetPeople, 
+  findFriends
 }
