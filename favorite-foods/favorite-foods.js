fc6f5a884477ff000a37bcdfe74310a897d4ef0a
Artemy Gibson <artemygibson@gmail.com>
2024-03-28 11:48:10 -0700

    Write all functions to pass favorite-foods tests

--- favorite-foods/favorite-foods.js
+++ favorite-foods/favorite-foods.js
@@ -1,4 +1,37 @@
+function createFavoriteFood(dish) {
+  var food = {
+    name: dish.dish,
+    ingredients: dish.ingredients,
+    isSpicy: dish.isSpicy,
+    timesOrdered: 0
+  }
+  return food;
+}
 
+function commentOnSpiciness(food) {
+  if (food.isSpicy) {
+    return `Wow, this ${food.name} is so spicy!`
+  } else {
+    return `Phew, this ${food.name} is not very spicy.`
+  }
+}
 
+function orderFood(food) {
+  food.timesOrdered += 1;
+  return food;
+}
 
-module.exports = { };
+function createShoppingList(foods) {
+  list = []
+  foods.forEach(function(food) {
+    list.push(food.ingredients)
+  })
+  return list.flat();
+}
+
+module.exports = { 
+  createFavoriteFood,
+  commentOnSpiciness,
+  orderFood,
+  createShoppingList
+};
