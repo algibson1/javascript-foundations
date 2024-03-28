1e4e084fa284c6a4eab35a197aac6f070d01cfa5
Artemy Gibson <artemygibson@gmail.com>
2024-03-28 11:58:07 -0700

    Write all functions to pass video games exercise

--- video-games/video-games.js
+++ video-games/video-games.js
@@ -1,4 +1,41 @@
+function createPlayer(name, age, moveset) {
+  var player = {
+    name: name,
+    age: age,
+    moveset: moveset
+  }
+  return player;
+}
 
+function createLevel(name, players) {
+  var level = {
+    name: name,
+    players: players,
+    coins: 0,
+    lives: 3
+  }
+  return level;
+}
 
+function findCoins(level, coins) {
+  level.coins += coins;
+  if (level.coins >= 100) {
+    level.lives += 1;
+  }
+  return level;
+}
 
-module.exports = { };
+function defeatPlayer(level) {
+  level.lives -= 1;
+  if (level.lives === 0) {
+    return 'GAME OVER';
+  }
+  return level;
+}
+
+module.exports = { 
+  createPlayer,
+  createLevel,
+  findCoins,
+  defeatPlayer
+};
