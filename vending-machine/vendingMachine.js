40e361c3dbfb8fa02cfddc6c689bd7366bd55f1a
Artemy Gibson <artemygibson@gmail.com>
2024-04-23 18:40:04 -0700

    Add functions to pass all vending machine tests

--- vending-machine/vendingMachine.js
+++ vending-machine/vendingMachine.js
@@ -1,2 +1,36 @@
+function createItemStock(name, quantity, price) {
+  var itemStock = {
+    name: name || 'unknown',
+    quantity: quantity || 0,
+    price: price || 1.00
+  };
+  return itemStock;
+};
 
-module.exports = {}
+function makePurchase(item, money) {
+  if (item.quantity === 0) {
+    return "Sorry, there are none left"
+  } else if (item.price > money) {
+    return `Sorry, you need at least $${item.price} to make that purchase`
+  } else {
+    return `Here are your ${item.name}`
+  }
+}
+
+function collectChange(coins) {
+  return coins.reduce(function(total, num) { return total + num; });
+  // array.reduce(function(total, currentValue, currentIndex, arr), initialValue)
+  // works a little bit like recursion under the hood
+  // total - the initial value or the previously returned value of the function
+  // currentValue - for each element, like iterating
+  // currentIndex - optional - index of current element, if useful
+  // arr - optional - the array the current element belongs to
+  // initialValue - optional - a value to start with, if useful
+}
+
+
+module.exports = {
+  createItemStock,
+  makePurchase,
+  collectChange
+}
